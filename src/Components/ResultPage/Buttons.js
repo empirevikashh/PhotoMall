1111873fb9e81bff2dc1beaf0642a89058c8bd33
Vikash <empirevikash@gmail.com>
2024-01-21 14:03:25 +0530

    Dynamic button

--- src/Components/ResultPage/Buttons.js
+++ src/Components/ResultPage/Buttons.js
@@ -26,7 +26,7 @@ const Buttons = () => {
   return (
     <div className="flex overflow-hidden hover:overflow-x-scroll py-3 px-4 bg-[#F5F5F5] absolute w-full">
       {buttonArr.map((name) => (
-        <div className="mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black">
+        <div key={name} className="mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black">
           {name}
         </div>
       ))}
