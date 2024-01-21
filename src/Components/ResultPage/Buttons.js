d540105e02cb66876a448eb2d77f3fcefa3e7680
Vikash <empirevikash@gmail.com>
2024-01-21 11:53:18 +0530

    button Done

--- src/Components/ResultPage/Buttons.js
+++ src/Components/ResultPage/Buttons.js
@@ -1,29 +1,37 @@
-import React from 'react'
+import React from "react";
+const buttonArr = [
+  "Forest",
+  "Mountain",
+  "River",
+  "Sunshine",
+  "Lake",
+  "Tree",
+  "Computer",
+  "TechHub",
+  "CodeCraft",
+  "DigitalBreeze",
+  "OceanView",
+  "WaveTech",
+  "NatureHub",
+  "EcoTech",
+  "GreenSpace",
+  "TechVista",
+  "SunTech",
+  "OceanTech",
+  "MountainCode",
+  "EcoOcean",
+];
 
 const Buttons = () => {
   return (
-    <div className='flex overflow-hidden overflow-x-scroll py-3 px-4 bg-[#F5F5F5]'>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-        <div className='mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black'>Buttons</div>
-
+    <div className="flex overflow-hidden hover:overflow-x-scroll py-3 px-4 bg-[#F5F5F5] absolute w-full">
+      {buttonArr.map((name) => (
+        <div className="mr-8 bg-[#F5F5F5] p-2 px-8 text-black border rounded-md border-black">
+          {name}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
